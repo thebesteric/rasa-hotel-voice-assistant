75d44f4868e02c01af1f8a8db16e735a96afe71b
eric <whatisjava@hotmail.com>
2025-03-27 01:40:47 +0800

    init

--- speech_model_download.py
+++ speech_model_download.py
@@ -2,5 +2,8 @@ if __name__ == '__main__':
     from modelscope import snapshot_download
 
     # 调用 snapshot_download 函数下载指定模型
-    model_dir = snapshot_download('speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch')
+    model_dir = snapshot_download(
+        model_id='speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
+        cache_dir="./asserts/lib"
+    )
     print(f"模型已下载到: {model_dir}")
